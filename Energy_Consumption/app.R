e307d11fd17e8e308ff77d1fcc8852a70acde665
allenrench222 <33609729+allenrench222@users.noreply.github.com>
2019-02-23 16:42:50 -0500

    Add files via upload

--- Energy_Consumption/app.R
+++ Energy_Consumption/app.R
@@ -10,7 +10,11 @@ library(shiny)
 #
 
 #import the Data
-source("C:/Users/598522/Documents/Rprojects/Hackathon/Energy_Consumption/Source_Clean.R")
+source("Source_Clean.R")
+Data$Clean.Score<-Data$CO2/Data$KWH
+inds.excludeT<-which(Data$CATEGORY_ROLLUP=="Total")
+inds.exclude<-which(Data$CATEGORY_ROLLUP=="Exclude")
+Data.ex<-Data[-c(inds.exclude,inds.excludeT),]
 Country_Choice<-list()
 un.count<-as.character(unique(Data$COUNTRY))
 for(i in 1:length(unique(Data$COUNTRY))){
@@ -30,38 +34,58 @@ library(shiny)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-   
+
    # Application title
-   titlePanel("Old Faithful Geyser Data"),
+   titlePanel("Energy Consumption and Forecast"),
    
    # Sidebar with a slider input for number of bins 
    sidebarLayout(
       sidebarPanel(
         selectInput("Country","Select Country: ",Country_Choice),
-        selectInput("Category","Select Category",Category_Choice)
+        selectInput("Category","Select Category",Category_Choice),
+        selectInput("Visualize","Select Visualization Technique:",c("Raw Scores"="RawScores","Score Ratio"="ScoreRatio"))
       ),
       
       # Show a plot of the generated distribution
       mainPanel(
-         plotlyOutput("distPlot"),
-         plotlyOutput("distPlot2"),
-         tableOutput("tab1")
-      )
+        tabsetPanel(type = "tabs",
+                    tabPanel("outputs",
+                             plotlyOutput("distPlot"),
+                             plotOutput("distPlot2"),
+                             tableOutput("tab1")
+                    ),
+                    tabPanel("Summary",
+                             HTML('<p><img src="my_img.jpg"/></p>')
+                             )
+                    
+        ))
    )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-   
+   #total outputs
+  
    output$distPlot <- renderPlotly({
       # generate bins based on input$bins from ui.R
-      x    <-  Data$YEAR[which(Data$COUNTRY==input$Country & Data$CATEGORY_ROLLUP==input$Category)]
-      y    <- Data$KWH[which(Data$COUNTRY==input$Country & Data$CATEGORY_ROLLUP==input$Category)]
+      x    <-  Data$YEAR[which(Data$COUNTRY==input$Country & Data$CATEGORY_ROLLUP==input$Category & Data$KWH !=0)]
+      
+      if(input$Visualize=="ScoreRatio"){
+        y    <- Data$Clean.Score[which(Data$COUNTRY==input$Country & Data$CATEGORY_ROLLUP==input$Category & Data$KWH !=0)]
+      }else{
+        y    <- Data$KWH[which(Data$COUNTRY==input$Country & Data$CATEGORY_ROLLUP==input$Category & Data$KWH !=0)]
+      }
       data.cur<-data.frame("Year"=x,"Consumption"=y)
       
       
       # draw the histogram with the specified number of bins
-      p<-plot_ly(data.cur,x=~Year,y=~Consumption,mode="lines",trace="scatter")
+      p<-plot_ly(data.cur,x=~Year,y=~Consumption,mode="lines",trace="scatter",type = "scatter")
+      if(input$Visualize=="Score Ratio"){
+        p<-p %>% layout(yaxis = list(range = c(0,1)))
+      }else{
+        p<-p
+      }
+      
       p
       
   })
@@ -71,37 +95,27 @@ server <- function(input, output) {
    )
    
    
-   output$distPlot2 <- renderPlotly({
-     x    <-Data$YEAR[which(Data$COUNTRY==input$Country)]
-     y    <-Data$KWH[which(Data$COUNTRY==input$Country)]
-     z    <-Data$CATEGORY_ROLLUP[which(Data$COUNTRY==input$Country)]
+   output$distPlot2 <- renderPlot({
+     x    <-Data.ex$YEAR[which(Data.ex$COUNTRY==input$Country & Data.ex$KWH!=0)]
+     if(input$Visualize=="ScoreRatio"){
+       y    <-Data.ex$Clean.Score[which(Data.ex$COUNTRY==input$Country & Data.ex$KWH!=0)]
+     }else{
+       y  <-Data.ex$KWH[which(Data.ex$COUNTRY==input$Country & Data.ex$KWH!=0)]
+     }
+     z    <-Data.ex$CATEGORY_ROLLUP[which(Data.ex$COUNTRY==input$Country & Data.ex$KWH!=0)]
      cat.cur<-unique(z)
      x.new<-x[which(z==cat.cur[1])]
      y.new<-y[which(z==cat.cur[1])]
      cur.data<-data.frame("Year"=x.new,"Consumption"=y.new)
-     
+     plot(cur.data$Year,cur.data$Consumption,xlim = c(1985,2030),ylim = c(0,max(y)),type = 'l')
+     legend("topright","(x,y)",cat.cur,lty=rep(1,length(cat.cur)),lwd=rep(2.5,length(cat.cur)),col=seq(1:length(cat.cur)))
      p<-plot_ly(cur.data, x = ~Year, y = ~Consumption, type = 'scatter', mode = 'lines',name = cat.cur[1])
      for(j in 2:length(cat.cur)){
        x.new<-x[which(z==cat.cur[j])]
        y.new<-y[which(z==cat.cur[j])]
-       
-       p<-add_trace(p, y=~y.new, x=~x.new, mode="lines",name=cat.cur[j],trace="scatter")
-       
-     }
-     
-     # a <- list(
-     #   autotick = FALSE,
-     #   ticks = "outside",
-     #   tick0 = 0,
-     #   dtick = 0.25,
-     #   ticklen = 5,
-     #   tickwidth = 2,
-     #   tickcolor = toRGB("blue")
-     # )
-     p<-p %>%
-       layout(yaxis=list(range=c(0,max(y))))
-     p
-     
+       cur.data<-data.frame("Year"=x.new,"Consumption"=y.new)
+       points(cur.data$Year,cur.data$Consumption,type='l',col=j)
+}
    })
 }
 
